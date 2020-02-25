76bfb4b13c5d39db103fdd778ef545b48bbaf8e5
刘印 <1604120269@qq.com>
2020-02-25 16:59:35 +0800

    模板

---
+++ MShopsApi/Model/GoodsTypeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MShopsApi.Model
+{
+    public class GoodsTypeModel
+    {
+    }
+}
